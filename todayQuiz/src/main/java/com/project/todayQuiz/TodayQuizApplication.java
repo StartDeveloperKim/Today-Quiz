449ed37257c049290edb9a4abba471c92fca262c
StartDeveloperKim <ktw550111@naver.com>
2023-04-17 14:43:15 +0900

    Feat. 오늘의 퀴즈 프로젝트 시작

---
+++ todayQuiz/src/main/java/com/project/todayQuiz/TodayQuizApplication.java
@@ -0,0 +1,13 @@
+package com.project.todayQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayQuizApplication.class, args);
+	}
+
+}
---
+++ todayQuiz/src/test/java/com/project/todayQuiz/TodayQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.todayQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
